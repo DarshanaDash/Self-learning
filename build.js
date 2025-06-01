e0a6653241ffd7a5fabc1ba8bc07f717f30a2ca2
ddas0813 <ddas0813@sydney.uni.edu.au>
2025-06-02 00:21:27 +1000

    reversing small error

--- build.js
+++
@@ -1,8 +0,0 @@
-// build.js
-const fs = require('fs');
-
-const template = fs.readFileSync('index.html.template', 'utf8');
-const replaced = template.replace(/%MY_API_KEY%/g, process.env.MY_API_KEY);
-
-fs.mkdirSync('dist', { recursive: true });
-fs.writeFileSync('dist/index.html', replaced);
